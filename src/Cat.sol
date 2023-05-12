efa4ed9b88873cc6dba7ef95a3f643af1f1965e0
Michael Reynolds <103044686+mireynolds@users.noreply.github.com>
2023-05-12 21:32:23 +0100

    Adding uma dependencies

--- src/Cat.sol
+++ src/Cat.sol
@@ -2,7 +2,11 @@
 pragma solidity 0.8.16;
 
 // Interfaces
-import "./interfaces/ICat.sol";
+import {ICat} from "./interfaces/ICat.sol";
+import {OptimisticOracleV3Interface} from "./interfaces/OptimisticOracleV3Interface.sol";
+
+// Libraries
+import {AncillaryData as ClaimData} from "./libraries/AncillaryData.sol";
 
 contract Cat is ICat {
 }
