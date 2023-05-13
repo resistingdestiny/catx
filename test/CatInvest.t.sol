2ed9ce8eefddef70872a67672ee128304fe59d1a
Michael Reynolds <103044686+mireynolds@users.noreply.github.com>
2023-05-14 00:00:49 +0100

    CatFactory: 0x20C6E292F4127e0FAB75AB67C01eD2Cb72fD814D Cat: 0xbCBC942D38c2b782A837C6e630dFD506a7EC421A

--- test/CatInvest.t.sol
+++ test/CatInvest.t.sol
@@ -10,53 +10,56 @@ import "../src/interfaces/ICat.sol";
 import "../src/mocks/mockERC20.sol";
 import "forge-std/console.sol";
 
-contract CatInvest is Test {
-    Cat singleton; // dont know if needed
-    Cat catContract;
+import "@openzeppelin/contracts/utils/math/Math.sol";
+import {ERC1155Holder} from "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
+
+contract CatInvest is Test, ERC1155Holder {
+    CatFactory factory;
+    Cat policy;
     Mock mockErc20;
 
     function setUp() public {
-        catContract = new Cat();
+        factory = new CatFactory();
         mockErc20 = new Mock();
     }
 
     function test_Invest(
-        address owner,
-        address receiver,
         uint[3] calldata amounts
+
     ) public {
+        ICat.Policy memory policyStruct = ICat.Policy({
+            name: "Twiser",
+            filecoinCID: "448jngf10248ut14tgj10g1gt41",
+            expiry: block.timestamp + 52 weeks,
+            holder: address(this),
+            catType: "0x",
+            paymentFrequency: 4 weeks,
+            size: 10000*10**18,
+            underlying: address(mockErc20),
+            statement: "hello",
+            whatThreeWords: "helloCatLion",
+            radius: "fifty",
+            category: ["easy", "medium", "bad"],
+            premiums: [uint(100), uint(200), uint(300)]
+        });
+        policy = Cat(factory.createPolicy(policyStruct));
         uint[] memory classList = new uint[](3);
-        // uint[] memory amountsList = new uint[](3);
         address[] memory addressThisList = new address[](3);
         for (uint i = 0; i < 3; i++) {
             classList[i] = i;
             addressThisList[i] = address(this);
         }
-        // address[] memory addressThisList = [
-        //     address(this),
-        //     address(this),
-        //     address(this)
-        // ];
-        // uint256[] memory classList = [0, 1, 2];
-        vm.assume(amounts[0] < mockErc20.totalSupply() / 3);
-        vm.assume(amounts[1] < mockErc20.totalSupply() / 3);
-        vm.assume(amounts[2] < mockErc20.totalSupply() / 3);
-        vm.assume(
-            amounts[0] <
-                catContract.balanceOfBatch(addressThisList, classList)[0]
-        );
-        vm.assume(
-            amounts[1] <
-                catContract.balanceOfBatch(addressThisList, classList)[1]
-        );
-        vm.assume(
-            amounts[2] <
-                catContract.balanceOfBatch(addressThisList, classList)[2]
-        );
-        catContract.invest(owner, receiver, amounts);
+        uint[] memory bondsAvailable = policy.balanceOfBatch(addressThisList, classList);
+        uint thirdSupply = mockErc20.totalSupply() / 3 ;
+        for (uint i; i < 3; i++) {
+            vm.assume(amounts[i] <= Math.min(thirdSupply, bondsAvailable[i]));
+        }
+        uint totalAmounts = amounts[0] + amounts[1] + amounts[2];
+        // Approve tokens
+        mockErc20.approve(address(policy), totalAmounts);
+        policy.invest(address(this), [amounts[0], amounts[1], amounts[2]]);
         assert(
-            mockErc20.balanceOf(address(this)) ==
-                amounts[0] + amounts[1] + amounts[2]
+            mockErc20.balanceOf(address(policy)) == totalAmounts
         );
     }
 }
